a9a4cbdfb96f7de2745cdf6c9dd9bb9b09bb313f
Danushka-Madushan <danushkamadushan128@gmail.com>
2026-09-18 05:34:53 +0530

    create 4 files, update 3 files and delete 2 files

--- client/app/build.gradle.kts
+++ client/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "nibm.iot.socketman"
-        minSdk = 28
+        minSdk = 29
         targetSdk = 37
         versionCode = 1
         versionName = "1.0"
---
+++ client/check.kt
@@ -0,0 +1,1 @@
+package nibm.iot.socketman
